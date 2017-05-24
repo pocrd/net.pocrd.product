e6cefb6c0eecb37c8bbfaf550c8ca96243abe513
rendong <leorendong@hotmail.com>
2017-05-24 11:38:24 +0800

    使用ExceptionHandler来捕获业务异常

--- api/src/main/java/net/pocrd/product/api/DemoService.java
+++ api/src/main/java/net/pocrd/product/api/DemoService.java
@@ -1,14 +1,13 @@
 package net.pocrd.product.api;
 
+import net.pocrd.annotation.*;
 import net.pocrd.define.AutowireableParameter;
+import net.pocrd.define.SecurityType;
+import net.pocrd.entity.ServiceException;
 import net.pocrd.product.entity.DemoEntity;
 import net.pocrd.product.entity.DemoEnum;
 import net.pocrd.product.entity.DemoReturnCode;
 import net.pocrd.product.mock.MockDemoEntity;
-import net.pocrd.annotation.*;
-import net.pocrd.define.CommonParameter;
-import net.pocrd.define.SecurityType;
-import net.pocrd.entity.ServiceException;
 import net.pocrd.util.RawString;
 
 @ApiGroup(name = "demo", minCode = 1, maxCode = 1000000, codeDefine = DemoReturnCode.class, owner = "demo")
@@ -17,27 +16,27 @@ public interface DemoService {
              security = SecurityType.None, owner = "demo")
     public DemoEntity sayHello(
             @ApiParameter(required = true, name = "name", desc = "say hello多语言测试. \nen-us:multi-language test \nja-jp:多言語テスト")
-            String name) throws ServiceException;
+                    String name) throws ServiceException;
 
     @HttpApi(name = "demo.testMock", desc = "mock test多语言测试. \nen-us:multi-language test \nja-jp:多言語テスト",
              security = SecurityType.None, owner = "demo")
     @ApiMockReturnObject(MockDemoEntity.class)
     public DemoEntity testMock(
             @ApiParameter(required = true, name = "name", desc = "say hello多语言测试. \nen-us:multi-language test \nja-jp:多言語テスト")
-            String name) throws ServiceException;
+                    String name) throws ServiceException;
 
     @HttpApi(name = "demo.testShortCircuit", desc = "short circuit test多语言测试. \nen-us:multi-language test \nja-jp:多言語テスト",
              security = SecurityType.None, owner = "demo")
     @ApiShortCircuit(MockDemoEntity.class)
     public DemoEntity testShortCircuit(
             @ApiParameter(required = true, name = "name", desc = "say hello多语言测试. \nen-us:multi-language test \nja-jp:多言語テスト")
-            String name) throws ServiceException;
+                    String name) throws ServiceException;
 
     @HttpApi(name = "demo.tryError", desc = "demo error多语言测试. \nen-us:multi-language test \nja-jp:多言語テスト",
              security = SecurityType.None, owner = "demo")
     public DemoEntity tryError(
             @ApiParameter(required = true, name = "in", desc = "input param多语言测试. \nen-us:multi-language test \nja-jp:多言語テスト")
-            String in) throws ServiceException;
+                    String in) throws ServiceException;
 
     @HttpApi(name = "demo.testRegistedDevice", desc = "demo registed device多语言测试. \nen-us:multi-language test \nja-jp:多言語テスト",
              security = SecurityType.RegisteredDevice, owner = "demo")
@@ -52,24 +51,24 @@ public interface DemoService {
              security = SecurityType.Integrated, owner = "demo")
     public String getResByThirdPartyId(
             @ApiAutowired(AutowireableParameter.thirdPartyId)
-            String thirdPartyId,
+                    String thirdPartyId,
             @ApiParameter(required = true, name = "in", desc = "输入参数多语言测试. \nen-us:multi-language test \nja-jp:多言語テスト")
-            String something) throws ServiceException;
+                    String something) throws ServiceException;
 
     @HttpApi(name = "demo.testRedirect", desc = "test redirect多语言测试. \nen-us:multi-language test \nja-jp:多言語テスト",
              security = SecurityType.None, owner = "demo")
     public RawString testRedirect(
             @ApiParameter(required = true, name = "something", desc = "输入参数1多语言测试. \nen-us:multi-language test \nja-jp:多言語テスト")
-            DemoEnum something,
+                    DemoEnum something,
             @EnumDef(DemoEnum.class)
             @ApiParameter(required = true, name = "another", desc = "输入参数2多语言测试. \nen-us:multi-language test \nja-jp:多言語テスト")
-            String another
+                    String another
     ) throws ServiceException;
 
     @HttpApi(name = "demo.testIgnoreParameterForSecurity", desc = "test redirect多语言测试. \nen-us:multi-language test \nja-jp:多言語テスト",
              security = SecurityType.None, owner = "demo")
     public String testIgnoreParameterForSecurity(
             @ApiParameter(required = true, name = "in", desc = "输入参数多语言测试. \nen-us:multi-language test \nja-jp:多言語テスト", ignoreForSecurity = true)
-            String something
+                    String something
     ) throws ServiceException;
 }
--- service/src/main/java/net/pocrd/product/service/http/DemoServiceImpl.java
+++ service/src/main/java/net/pocrd/product/service/http/DemoServiceImpl.java
@@ -9,14 +9,13 @@ import net.pocrd.product.api.DemoService;
 import net.pocrd.product.dao.entity.DemoDTO;
 import net.pocrd.product.entity.DemoEntity;
 import net.pocrd.product.entity.DemoEnum;
-import net.pocrd.product.entity.DemoReturnCode;
 import net.pocrd.util.EvaluaterProvider;
 import net.pocrd.util.RawString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DemoServiceImpl implements DemoService {
-    private static final Logger logger = LoggerFactory.getLogger(DemoServiceImpl.class);
+    private static final Logger    logger    = LoggerFactory.getLogger(DemoServiceImpl.class);
     private static final Evaluater evaluater = EvaluaterProvider.getEvaluater(DemoEntity.class, DemoDTO.class);
     //@Autowired
     //private DemoDTOMapper demoDTOMapper;
@@ -25,42 +24,31 @@ public class DemoServiceImpl implements DemoService {
      * 边界异常处理
      *
      * @param name
+     *
      * @throws ServiceException
      */
     @Override
-    public DemoEntity sayHello(String name) throws ServiceException {
+    public DemoEntity sayHello(String name) {
         DemoEntity result = null;
-        try {
-            result = new DemoEntity();
-            result.id = 1;
-            result.name = name;
-        } catch (ServiceRuntimeException sre) {
-            logger.error("api failed.", sre);
-            throw new ServiceException("api failed.", sre);
-        } catch (Throwable t) {
-            logger.error("api failed.", t);
-            if (t instanceof ServiceException) {
-                throw (ServiceException)t;
-            } else {
-                throw new ServiceException(DemoReturnCode.DEMO_UNKNOW_ERROR, "api failed.");
-            }
-        }
+        result = new DemoEntity();
+        result.id = 1;
+        result.name = name;
         return result;
     }
 
     @Override
-    public DemoEntity testMock(String name) throws ServiceException {
+    public DemoEntity testMock(String name) {
         return null;
     }
 
     @Override
-    public DemoEntity testShortCircuit(String name) throws ServiceException {
+    public DemoEntity testShortCircuit(String name) {
         return null;
     }
 
     @Override
-    public DemoEntity tryError(String in) throws ServiceException {
-        throw new ServiceException(ApiReturnCode.API_UPGRADE, "try error!");
+    public DemoEntity tryError(String in) {
+        throw new ServiceRuntimeException(ApiReturnCode.API_UPGRADE, "try error!");
     }
 
     @Override
@@ -69,26 +57,26 @@ public class DemoServiceImpl implements DemoService {
     }
 
     @Override
-    public String testUserLogin() throws ServiceException {
+    public String testUserLogin() {
         DubboExtProperty.ClientCaller caller = DubboExtProperty.getClientCallerFromAttachment();
         System.out.println("deviceId:" + caller.deviceId + ", userId:" + caller.userId);
         return "deviceId:" + caller.deviceId + ", userId:" + caller.userId;
     }
 
     @Override
-    public String getResByThirdPartyId(String thirdPartyId, String something) throws ServiceException {
+    public String getResByThirdPartyId(String thirdPartyId, String something) {
         System.out.println("something:" + something);
         return something;
     }
 
     @Override
-    public RawString testRedirect(DemoEnum something, String other) throws ServiceException {
+    public RawString testRedirect(DemoEnum something, String other) {
         DubboExtProperty.setRedirectUrl("http://www.fengqu.com/info.api");
         return new RawString("testRedirect");
     }
 
     @Override
-    public String testIgnoreParameterForSecurity(String something) throws ServiceException {
+    public String testIgnoreParameterForSecurity(String something) {
         System.out.println("something:" + something);
         try {
             Thread.sleep(300);
